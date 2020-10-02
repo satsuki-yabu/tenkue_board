ae1b28ff9e7bc89994d31cb4516d6135831e2f9a
satsuki-y <tetecoo.1019@gmail.com>
2020-10-02 18:22:19 +0900

    new_post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to :action
+  end
+
+  def edit
+  end
+end
+
+
+private
+    def post_params
+        params.require(:post).permit(:content)
+    end
