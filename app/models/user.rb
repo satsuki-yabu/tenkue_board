6cab4fb417e8cd41097a7e2d9231a8ef1a346d74
satsuki-y <tetecoo.1019@gmail.com>
2020-10-06 07:36:49 +0900

    ログイン、ログアウトのリンクをヘッダーにつける

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :microposts
+  has_many :posts, dependent: :destroy
+ 
+  validates :name, presence: true
+  before_save   :downcase_email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+
+  def downcase_email
+    self.email = email.downcase
+  end
 end
