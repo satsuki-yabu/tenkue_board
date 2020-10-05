65c43be1876769dbaa7f79c44db5c9c8161dbba6
satsuki-y <tetecoo.1019@gmail.com>
2020-10-06 08:35:24 +0900

    postコントローラ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params) 
+    @post = current_user.posts.build(post_params)
       if @post.save
         redirect_to action: :index
       else
