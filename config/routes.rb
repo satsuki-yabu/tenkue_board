e341541fa0a32b2bf6b624588f69afbb57f506a7
satsuki-y <tetecoo.1019@gmail.com>
2020-10-07 21:48:24 +0900

    不要なルーティングを削除した

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-    get 'users/sign_out', to: 'devise/sessions#destroy'
   end
 
   resources :posts
