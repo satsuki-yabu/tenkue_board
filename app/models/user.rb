371d9c87afcc2be9e3d75643ec27ade00c4f9ef1
satsuki-y <tetecoo.1019@gmail.com>
2020-10-07 19:27:35 +0900

    userモデルのバリデーションを削除した

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
- 
-  validates :name, presence: true
-  before_save   :downcase_email
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }}
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-
-  def downcase_email
-    self.email = email.downcase
-  end
 end
