51f1d496776590a319f5ed45a969c096c0b723f2
satsuki-yabu <tetecoo.1019@gmail.com>
2020-12-02 19:07:28 +0900

    モデル定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy 
+  has_many :favorites, dependent: :destroy 
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
 
   validates :name, presence: true, uniqueness: true
 
