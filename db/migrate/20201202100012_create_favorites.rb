f5a6aa7f8d846c3436964b7a62459ae9a3c5bcb6
satsuki-yabu <tetecoo.1019@gmail.com>
2020-12-02 19:04:36 +0900

    favorite table作成

--- db/migrate/20201202100012_create_favorites.rb
+++ db/migrate/20201202100012_create_favorites.rb
@@ -1,7 +1,6 @@
-class CreateComments < ActiveRecord::Migration[5.2]
+class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
-    create_table :comments do |t|
-      t.string :comment
+    create_table :favorites do |t|
       t.references :user, foreign_key: true
       t.references :post, foreign_key: true
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_011511) do
+ActiveRecord::Schema.define(version: 2020_12_02_100012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_11_08_011511) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_favorites_on_post_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -48,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_011511) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "posts"
+  add_foreign_key "favorites", "users"
   add_foreign_key "posts", "users"
 end
