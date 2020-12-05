0e92db473a84e93b5daea79cf02bab176fdb9954
satsuki-yabu <tetecoo.1019@gmail.com>
2020-12-05 17:05:39 +0900

    favoriteコントローラに、create,deleteメソッドを実装

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,24 @@
+class FavoritesController < ApplicationController
+  before_action :set_post
+  before_action :authentcate_user.id
+
+  def create
+      current_user.favorites.create(post_id: @post.id).delete
+  end
+
+  def destroy
+    current_user.favorites.find_by(id:params[:id]).delete
+  end
+
+private
+  def favorited_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def log_in_user?
+    unless user_signed_in?
+      flash[:alert] = "新規登録またはユーザーログインが必要です"
+      redirect_to new_user_session_path
+    end
+  end
+end
