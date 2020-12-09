97fad7138247371b9dccbb975696108858cfbdc6
satsuki-yabu <tetecoo.1019@gmail.com>
2020-12-10 05:18:01 +0900

    コード修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,8 @@ class Post < ApplicationRecord
 
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+
+  def liked_by?(user)
+    self.likes.exists?(user_id: user.id)
+  end
 end
